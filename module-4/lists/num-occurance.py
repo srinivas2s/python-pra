be5e455ecd8cc28664f800ac338415b9a1a90134
Srinivas P <srinivas2006.srini@gmail.com>
2026-05-08 12:00:00 +0530

    dd

---
+++ module-4/lists/num-occurance.py
@@ -0,0 +1,18 @@
+file = open("input.txt", "r")
+data = file.read()
+file.close()
+
+digits = set()
+print(digits)
+special = {}
+
+for ch in data:
+    if ch.isdigit():
+        digits.add(ch)
+
+    elif not ch.isalnum() and not ch.isspace():
+        special[ch] = special.get(ch, 0) + 1
+
+print("Unique Digits Count =", len(digits))
+print("Digits =", digits)
+print("Special Characters =", special)
